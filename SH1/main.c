2c2dc402fcb85e948b089168bcc3fd7c6e9e5480
Alexandre FOLLIN <afollin@e1r6p8.42.fr>
2015-04-28 15:08:15 +0200

    norm 75%

--- SH1/main.c
+++ SH1/main.c
@@ -13,10 +13,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "libft.h"
-#include <unistd.h> // chdir, execve
-#include <sys/wait.h> // wait
+#include <unistd.h>
+#include <sys/wait.h>
 
-char	*ft_strcat_in_new_str(char *s1, char *s2)
+char		*ft_strcat_in_new_str(char *s1, char *s2)
 {
 	int			i;
 	int			sz_1;
@@ -33,7 +33,7 @@ char	*ft_strcat_in_new_str(char *s1, char *s2)
 	return (ret);
 }
 
-char    *duplicate_str(char *str)
+char		*duplicate_str(char *str)
 {
 	char		*dup;
 	int			str_size;
@@ -48,18 +48,19 @@ char    *duplicate_str(char *str)
 	return (dup);
 }
 
-int			cmp_spe(char *s1, char *s2, char ref){
+int			cmp_spe(char *s1, char *s2, char ref)
+{
 	int		i;
 	int		j;
-	
+
 	i = -1;
 	while (s1[++i] && s1[i] != ref)
 		;
 	if (!s1[i])
 		return (-1);
 	j = -1;
-
-	while (++j < i){
+	while (++j < i)
+	{
 		if (s1[j] != s2[j])
 			return (-1);
 	}
@@ -75,7 +76,8 @@ char		**cp_tab_str(char **tab)
 	int			count;
 
 	ret = NULL;
-	if (tab == NULL || tab[0] == NULL){
+	if (tab == NULL || tab[0] == NULL)
+	{
 		ret = (char **)malloc(sizeof(char *) * 1);
 		ret[0] = NULL;
 		return (ret);
@@ -91,9 +93,9 @@ char		**cp_tab_str(char **tab)
 	return (ret);
 }
 
-void    print_tab_str(char **tab)
+void		print_tab_str(char **tab)
 {
-	int             i;
+	int		i;
 
 	i = -1;
 	while (tab && tab[++i])
@@ -107,8 +109,8 @@ void		early_exit(int argc, char **argv)
 
 	i = -1;
 	argc = 0;
-	ft_putstr("usage: ");
-	ft_putendl(argv[0]);
+	ft_putstr_fd("usage: ", 2);
+	ft_putendl_fd(argv[0], 2);
 	exit(0);
 	return ;
 }
@@ -134,6 +136,25 @@ int			count_word(char *str)
 	return (count);
 }
 
+int			additionnal_gwx(char *str, int x, int *i)
+{
+	int		count;
+
+	count = 0;
+	while (str[*i] && count < x)
+	{
+		while (str[*i] == ' ' || str[*i] == '	')
+			++(*i);
+		while (str[*i] && str[*i] != ' ' && str[*i] != '	')
+		{
+			++(*i);
+			if (!str[*i] || str[*i] == ' ' || str[*i] == '	')
+				++count;
+		}
+	}
+	return (count);
+}
+
 char		*get_word_x(char *str, int x)
 {
 	char		*ret;
@@ -143,19 +164,7 @@ char		*get_word_x(char *str, int x)
 
 	ret = NULL;
 	i = 0;
-	count = 0;
-	while (str[i] && count < x)
-	{
-		while (str[i] == ' ' || str[i] == '	')
-			++i;
-		while (str[i] && str[i] != ' ' && str[i] != '	')
-		{
-			++i;
-			if (!str[i] || str[i] == ' ' || str[i] == '	')
-				++count;
-		}
-
-	}
+	count = additionnal_gwx(str, x, &i);
 	while (str[i] == ' ' || str[i] == '	')
 		++i;
 	j = i;
@@ -169,6 +178,25 @@ char		*get_word_x(char *str, int x)
 	return (ret);
 }
 
+int			additionnal_gwx_spe(char *str, int x, char c, int *i)
+{
+	int		count;
+
+	count = 0;
+	while (str[*i] && count < x)
+	{
+		while (str[*i] == c)
+			++(*i);
+		while (str[*i] && str[*i] != c)
+		{
+			++(*i);
+			if (!str[*i] || str[*i] == c)
+				++count;
+		}
+	}
+	return (count);
+}
+
 char		*get_word_x_spe(char *str, int x, char c)
 {
 	char		*ret;
@@ -178,18 +206,7 @@ char		*get_word_x_spe(char *str, int x, char c)
 
 	ret = NULL;
 	i = 0;
-	count = 0;
-	while (str[i] && count < x)
-	{
-		while (str[i] == c)
-			++i;
-		while (str[i] && str[i] != c)
-		{
-			++i;
-			if (!str[i] || str[i] == c)
-				++count;
-		}
-	}
+	count = additionnal_gwx_spe(str, x, c, &i);
 	if (!str[i] && count < x)
 		return (NULL);
 	while (str[i] == c)
@@ -205,6 +222,47 @@ char		*get_word_x_spe(char *str, int x, char c)
 	return (ret);
 }
 
+char		**additionnal_ate_1(int *count, char **tab)
+{
+	char	**ret;
+
+	ret = (char **)malloc(sizeof(char *) * 2);
+	ret[1] = NULL;
+	*count = 0;
+	if (tab != NULL)
+		free(tab);
+	return (ret);
+}
+
+char		**additionnal_ate_2(char **tab, char *key, char *value, int *count)
+{
+	char		**ret;
+	char		*tmp;
+	int			tab_len;
+	int			flag_key;
+
+	tab_len = -1;
+	flag_key = 1;
+	while (tab[++tab_len] && tab[tab_len][0])
+		flag_key = !cmp_spe(tab[tab_len], key, '=') ? 0 : flag_key;
+	ret = (char **)malloc(sizeof(char *) * (tab_len + 1 + flag_key));
+	ret[tab_len + flag_key] = NULL;
+	*count = -1;
+	while (++(*count) < tab_len)
+	{
+		if (cmp_spe(tab[*count], key, '='))
+			ret[*count] = duplicate_str(tab[*count]);
+		else
+		{
+			tmp = ft_strcat_in_new_str(key, "=");
+			ret[*count] = ft_strcat_in_new_str(tmp, value);
+		}
+		free(tab[*count]);
+	}
+	free(tab);
+	return (ret);
+}
+
 char		**add_to_env(char **tab, char *keyword, char *value)
 {
 	char		**ret;
@@ -215,33 +273,17 @@ char		**add_to_env(char **tab, char *keyword, char *value)
 
 	flag_key = 1;
 	ret = NULL;
-	if (tab == NULL || tab[0] == NULL){
-		ret = (char **)malloc(sizeof(char *) * 2);
-		ret[1] = NULL;
-		count = 0;
-		if (tab != NULL)
-			free(tab);
-	}
-	else {
+	if (tab == NULL || tab[0] == NULL)
+		ret = additionnal_ate_1(&count, tab);
+	else
+	{
 		tab_len = -1;
 		while (tab[++tab_len] && tab[tab_len][0])
 			flag_key = !cmp_spe(tab[tab_len], keyword, '=') ? 0 : flag_key;
-		ret = (char **)malloc(sizeof(char *) * (tab_len + 1 + flag_key));
-		ret[tab_len + flag_key] = NULL;
-		count = -1;
-		while (++count < tab_len)
-		{
-			if (cmp_spe(tab[count], keyword, '='))
-				ret[count] = duplicate_str(tab[count]);
-			else{
-				tmp = ft_strcat_in_new_str(keyword, "=");
-				ret[count] = ft_strcat_in_new_str(tmp, value);
-			}
-			free(tab[count]);
-		}
-		free(tab);
+		ret = additionnal_ate_2(tab, keyword, value, &count);
 	}
-	if (flag_key == 1){
+	if (flag_key == 1)
+	{
 		tmp = ft_strcat_in_new_str(keyword, "=");
 		ret[count] = ft_strcat_in_new_str(tmp, value);
 		free(tmp);
@@ -249,22 +291,25 @@ char		**add_to_env(char **tab, char *keyword, char *value)
 	return (ret);
 }
 
-char	**remove_from_env(char **env, char *keyword){
-	int 		tab_len;
-	int 		flag_key;
-	int 		count;
-	int 		index;
-	char		**ret;
+char		**remove_from_env(char **env, char *keyword)
+{
+	int		tab_len;
+	int		flag_key;
+	int		count;
+	int		index;
+	char	**ret;
 
 	flag_key = -1;
-	if (env == NULL || env[0] == NULL){
+	if (env == NULL || env[0] == NULL)
+	{
 		ft_putendl_fd("env is null, nothing to remove from env", 2);
-		return env;
+		return (env);
 	}
 	tab_len = -1;
 	while (env[++tab_len] && env[tab_len][0])
 		flag_key = !cmp_spe(env[tab_len], keyword, '=') ? tab_len : flag_key;
-	if (flag_key == -1){
+	if (flag_key == -1)
+	{
 		ft_putendl_fd("keyword not found in env", 2);
 		return (env);
 	}
@@ -273,14 +318,13 @@ char	**remove_from_env(char **env, char *keyword){
 	ret[tab_len - 1] = NULL;
 	count = -1;
 	index = -1;
-	while (++count < tab_len){
+	while (++count < tab_len)
+	{
 		if (cmp_spe(env[count], keyword, '='))
 			ret[++index] = duplicate_str(env[count]);
 		free(env[count]);
 	}
 	free(env);
-
-
 	return (ret);
 }
 
@@ -320,24 +364,26 @@ char		**do_unsetenv(char *line, char **env)
 char		**env_stuff(char *line, int *f, char **env)
 {
 	if (*f)
-		return env;
-	if (!ft_strcmp(line, "env")){
+		return (env);
+	if (!ft_strcmp(line, "env"))
+	{
 		print_tab_str(env);
 		*f = 1;
 	}
-	else if (!ft_strncmp(line, "setenv", 6)){
+	else if (!ft_strncmp(line, "setenv", 6))
+	{
 		env = do_setenv(line, env);
 		*f = 1;
 	}
-	else if (!ft_strncmp(line, "unsetenv", 8)){
+	else if (!ft_strncmp(line, "unsetenv", 8))
+	{
 		env = do_unsetenv(line, env);
 		*f = 1;
 	}
-
 	return (env);
 }
 
-char 		*get_env_var(char **env, char *var)
+char		*get_env_var(char **env, char *var)
 {
 	int		i;
 
@@ -352,18 +398,17 @@ char 		*get_env_var(char **env, char *var)
 
 char		**cd_stuff(char *line, int *f, char **env)
 {
-	int 		ret;
-	int 		flag_key;
-	char 		buf[256];
-	char 		*word_1;
-	char 		*word_2;
+	int		ret;
+	char	buf[256];
+	char	*word_1;
+	char	*word_2;
 
-	flag_key = -1;
 	if (*f)
 		return (env);
 	word_1 = get_word_x(line, 0);
 	ret = -1;
-	if (ft_strcmp(word_1, "cd")){
+	if (ft_strcmp(word_1, "cd"))
+	{
 		free(word_1);
 		return (env);
 	}
@@ -375,39 +420,36 @@ char		**cd_stuff(char *line, int *f, char **env)
 	}
 	ret = -1;
 	word_2 = get_word_x(line, 1);
-	if (count_word(line) == 1){
-		// get env var home s value;
-		while (env[++ret] && env[ret][0])
-			flag_key = !cmp_spe(env[ret], "HOME", '=') ? ret : flag_key;
-		if (flag_key == -1)
-			return (env);
-		ret = chdir(env[flag_key] + 5);
-	}
-	else if (!ft_strcmp(word_2, "-")){
-		ret = chdir(get_env_var(env, "OLDPWD") + 7);
-	}
-	else {
-		ret = chdir(word_2);
-	}
 	getcwd(buf, 256);
-	env = add_to_env(env, "OLDPWD", buf);
+	if (count_word(line) == 1)
+		ret = chdir(get_env_var(env, "HOME") + 5);
+	else if (!ft_strcmp(word_2, "-"))
+		ret = chdir(get_env_var(env, "OLDPWD") + 7);
+	else
+		ret = chdir(word_2);
 	if (ret == -1)
 		ft_putendl_fd("Impossible de changer de repertoire.", 2);
+	else
+	{
+		env = add_to_env(env, "OLDPWD", buf);
+		getcwd(buf, 256);
+		env = add_to_env(env, "PWD", buf);
+	}
 	free(word_1);
 	free(word_2);
 	*f = 1;
 	return (env);
-
 }
 
-char 		*find_exec_path(char *env_path, char *cmd) // attention a bien envoyer env + 5
+char		*find_exec_path(char *env_path, char *cmd)
 {
-	int 		i;
-	char 		*tmp;
-	char 		*tmp2;
+	int		i;
+	char	*tmp;
+	char	*tmp2;
 
 	i = -1;
-	while ((tmp = get_word_x_spe(env_path, ++i, ':')) != NULL){
+	while ((tmp = get_word_x_spe(env_path, ++i, ':')) != NULL)
+	{
 		tmp2 = ft_strcat_in_new_str(tmp, "/");
 		free(tmp);
 		tmp = ft_strcat_in_new_str(tmp2, cmd);
@@ -421,8 +463,8 @@ char 		*find_exec_path(char *env_path, char *cmd) // attention a bien envoyer en
 
 void		do_exec(char *path_to_exec, char **tab_str, char **env)
 {
-	pid_t		father;
-	int 		child_status;
+	pid_t	father;
+	int		child_status;
 
 	father = fork();
 	if (father > 0)
@@ -432,9 +474,10 @@ void		do_exec(char *path_to_exec, char **tab_str, char **env)
 	return ;
 }
 
-void		execute_stuff(char **tab_str, int *f, char **env){
-	char 	*path_to_exec;
-	int 	i;
+void		execute_stuff(char **tab_str, int *f, char **env)
+{
+	char	*path_to_exec;
+	int		i;
 
 	i = -1;
 	path_to_exec = NULL;
@@ -445,16 +488,17 @@ void		execute_stuff(char **tab_str, int *f, char **env){
 	if (!env[i])
 		return ;
 	path_to_exec = find_exec_path(env[i] + 5, tab_str[0]);
-	if (path_to_exec != NULL){
+	if (path_to_exec != NULL)
+	{
 		*f = 1;
 		do_exec(path_to_exec, tab_str, env);
 	}
 	return ;
 }
 
-int 		go_gnl(int i, char **line)
+int			go_gnl(int i, char **line)
 {
-	ft_putstr("\e[1;37m#$^&\e[0;31m42\e[1;37m*>> \e[0m");
+	ft_putstr("\033[1;37m#$^&\033[0;31m42\033[1;37m*>> \033[0m");
 	return (get_next_line(i, line));
 }
 
@@ -479,10 +523,9 @@ int			main(int argc, char **argv, char **env)
 	cute_flag = 0;
 	while (go_gnl(0, &line) > 0)
 	{
-		if (!line || !line[0]){
+		if (!line || !line[0])
 			continue ;
-		}
-		if (ft_strcmp(line,"exit") == 0)
+		if (ft_strcmp(line, "exit") == 0)
 			return (1);
 		cp_env = env_stuff(line, &cute_flag, cp_env);
 		cp_env = cd_stuff(line, &cute_flag, cp_env);
